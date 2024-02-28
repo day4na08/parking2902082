2d97178664f6c624e77e042186f787f5e5cae736
day4na08 <159964077+day4na08@users.noreply.github.com>
2024-02-28 00:56:59 -0500

    Update Cliente.java

--- src/main/java/org/ficha2902082/maven/parking/kathydayis/entities/Cliente.java
+++ src/main/java/org/ficha2902082/maven/parking/kathydayis/entities/Cliente.java
@@ -5,48 +5,42 @@ import java.util.List;
 
 
 public class Cliente {
+
     public String nombre;
     public String apellidos;
     public TipoDocumento tipoDocumento;
     public Long numeroDocumento;
     public Long celular;
+    public List<Carro> misCarros;
+
     
+                    
 
-    public List<Carro> misCarros = new ArrayList<Carro>();
-
-    // metodo 1: añadir carro al cliente
-    // definir metodo(firma del metodo - Signature):
-    // - modificador acceso
-    // - tipo de dato de retorno
-    // - nombre del metodo
-    // parametros(Entradas, inputs):
-    // 1. Tipo de dato del parametro
-    // 2. Nombre del parametro
-    public void addCar(Carro c) {
-        this.misCarros.add(c);
-    }
-
-    // sobrecarga del metodo addCar
-    public void addCar(String placa, TipoVehiculo tipoVehiculo) {
-        // construir(instanciar)
-        Carro c = new Carro();
-        c.placa = placa;
-        c.tipoVehiculo = tipoVehiculo;
-        
-        
-    }
-    // sobrecarga de metodos:
-    // en una clase se permiten metodos
-    // con el mismo nombre pero
-    // con diferente firma
-
-    public Cliente(String nombre, String apellidos, TipoDocumento tipoDocumento, Long numeroDocumento, Long celular) {
+    public Cliente(String nombre,
+                   String apellidos,
+                   TipoDocumento tipoDocumento,
+                   Long numeroDocumento,
+                   Long celular) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.tipoDocumento = tipoDocumento;
         this.numeroDocumento = numeroDocumento;
         this.celular = celular;
-    
-    
+        this.misCarros = new ArrayList<Carro>();
+    }
+    public void addCar(Carro c ){
+        this.misCarros.add(c);
+    }
+    public void addCar( String placa,
+           TipoVehiculo tipoVehiculo ){
+        Carro c = new Carro();
+        c.placa = placa;
+        c.tipoVehiculo = tipoVehiculo;
+        this.misCarros.add(c);
     }
 }
+
+   
+    
+
+
