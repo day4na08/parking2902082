a0570f0ef73d161729b1cc4b63433282be73792e
day4na08 <159964077+day4na08@users.noreply.github.com>
2024-02-28 01:00:15 -0500

    Update Main.java

--- src/main/java/org/ficha2902082/maven/parking/kathydayis/Main.java
+++ src/main/java/org/ficha2902082/maven/parking/kathydayis/Main.java
@@ -1,9 +1,8 @@
 package org.ficha2902082.maven.parking.kathydayis;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.*;
 
 import org.ficha2902082.maven.parking.kathydayis.entities.Carro;
 import org.ficha2902082.maven.parking.kathydayis.entities.Cliente;
@@ -16,49 +15,75 @@ import org.ficha2902082.maven.parking.kathydayis.entities.TipoVehiculo;
 
 
 public class Main {
+
     public static void main(String[] args) {
-          // crear dos instancias
-           // de la clase carro
-             //instanciar dos cupos//
-        Cupo Cupito1 = new Cupo();
-        Cupito1.ancho =1.0;
-        Cupito1.largo =4.5;
-        Cupito1.letra ='A';
         
-        Cupo Cupito2 = new Cupo('B', 2.5, 2.5);
-        System.out.println();
-                            
+        //Crear dos cupos
+        //Instanciar dos cupos: 
+        Cupo cupito1 = new Cupo();
+        cupito1.ancho = 1.0;
+        cupito1.largo = 4.5;
+        cupito1.letra = 'A';
 
-    Carro carrito1 = new Carro("xxa-123", TipoVehiculo.TAXI);
-    Carro carrito2= new Carro("jda-356", TipoVehiculo.BUS);
+        Cupo cupito2 = new Cupo('B' ,
+                                 5.0 ,
+                                  2.5 );
+
+        //Instanciar dos carros:
+        Carro carrito1 = new Carro("ASJ 456" , 
+                                  TipoVehiculo.MOTO);
+        Carro carrito2 = new Carro("DFR 234" ,
+                                  TipoVehiculo.CAMIONETA);
+
+        //Instanciar un cliente:
+        Cliente cli = new Cliente("Cristian",
+                                  "Buitrago", 
+                                  TipoDocumento.CC , 
+                                  25234234L, 
+                                  3005555555L);
+        
+        //vincular carros al cliente
+        cli.addCar(carrito1);
+        cli.addCar(carrito2);
+        cli.addCar("ERT TYU",
+                   TipoVehiculo.CAMION);
+
+        //Declara una lista de Registros
+        List<Registro> misRegistros = new ArrayList<>();
+
+        //Instanciar dos Registros E/S
+        Registro registro1 = new Registro(
+            LocalDate.of(2024 , Month.JANUARY , 23) , 
+            LocalTime.of(15 , 30 , 34) , 
+            LocalDate.of(2024 , Month.FEBRUARY , 1),
+            LocalTime.of(6 , 30 , 23), 
+            50000.00,
+            cli.misCarros.get(0),
+            cupito1
+        );
+
+        //vincular los registros a la lista
+        //de registros
+        misRegistros.add(registro1);
+
+        //Recorrer la lista de registros
+        for( Registro r : misRegistros  ){
+                System.out.println( "Placa: " +  
+                                    r.carro.placa  + "|" +
+                                    "Cupo: " + 
+                                    r.cupo.letra + "|" + 
+                                    "Valor: " + 
+                                    r.valor + "|" +
+                                    "Fecha Inicio: " + 
+                                    r.fechaInicio.toString() + "|"
+
+                );
+        }
+
+
+
+        
+    }
+
+ }
     
-
-    Cliente clientecito1 = new Cliente( "dayana", "aguilar", TipoDocumento.CC, 1673612735L, 61365657138L);
-    Cliente clientecito2 = new Cliente("juan", "Arias", TipoDocumento.CE, 63176473877213L, 6736814871L);
-
-    clientecito1.addCar(carrito2);
-    clientecito1.addCar("xxa-123", TipoVehiculo.TAXI);
-    
-
-    clientecito2.addCar(carrito1);
-
-
-    
-    
-    Cupito2.letra='B';
-
-    List <Registro> misRegistros = new ArrayList<>();
-    
-
-
-    Registro registro1 = new Registro(LocalDate.of(24, 03, 23), LocalTime.of(16, 04, 43), LocalDate.of(24, 03, 24), "45000", clientecito1.misCarros.get(0));
-    Registro registro2 = new Registro(LocalDate.of(24, 04, 03), LocalTime.of(12, 54, 45), LocalDate.of(24, 04, 05), "45000", clientecito2.misCarros.get(0));
-      
-
-    System.out.println("Placa" + r.Carro.Placa + "|" + "Cupo:" + r.cupo.letra);
-
-   
-}
-}
-//recorrer la lista de registros//
-//for(   Registro :  misRegistros )
