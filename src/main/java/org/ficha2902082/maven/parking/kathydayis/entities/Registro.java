79eb06b9ccfc2ef7ad264a03f27b91673dabb294
day4na08 <159964077+day4na08@users.noreply.github.com>
2024-02-28 00:58:11 -0500

    Update Registro.java

--- src/main/java/org/ficha2902082/maven/parking/kathydayis/entities/Registro.java
+++ src/main/java/org/ficha2902082/maven/parking/kathydayis/entities/Registro.java
@@ -1,5 +1,6 @@
 package org.ficha2902082.maven.parking.kathydayis.entities;
 
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -9,13 +10,27 @@ public class Registro {
     public LocalDate fechaFin;
     public LocalTime horaFin;
     public double valor;
-    public Registro(LocalDate fechaInicio, LocalTime horaInicio, LocalDate fechaFin, String string, Carro carro) {
+
+    public Carro carro;
+    public Cupo cupo;
+
+    
+
+    public Registro() {
+    }
+
+
+    public Registro(LocalDate fechaInicio, LocalTime horaInicio, LocalDate fechaFin, LocalTime horaFin, Double valor,
+            Carro carro, Cupo cupo) {
         this.fechaInicio = fechaInicio;
         this.horaInicio = horaInicio;
         this.fechaFin = fechaFin;
-        this.horaFin = String;
-        this.valor = carro;
+        this.horaFin = horaFin;
+        this.valor = valor;
+        this.carro = carro;
+        this.cupo = cupo;
     }
     
     
 }
+
